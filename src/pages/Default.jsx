60128a97fb17ba95577df6038550f2c4291adc3d
Onuoha Collins <onuohacollinschidubem@gmail.com>
2020-05-22 14:10:46 -0700

    new chsages

--- src/pages/Default.jsx
+++ src/pages/Default.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import Header from '../components/Header'
+import {Link} from 'react-router-dom'
 
 class Default extends Component {
     render() {
         return (
-            <div>
-                <h1>dafault error </h1>
-            </div>
+
+            <Header title="404"  StyleClass="default-hero">
+                <h2 className="text-light text-uppercase mb-4">
+                    you are in the wrong place
+              </h2>
+                <Link to="/" className="text-uppercase btn-secondary btn-lg mt-3 text-decoration-none">
+                    return home
+                 </Link>
+            </Header>
+
         );
     }
 }
